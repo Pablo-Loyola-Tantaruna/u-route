a381360713caabfa17552b12b5430bf2bde604c1
Pablo Loyola <ployola@hiper.com.pe>
2023-03-04 01:37:21 -0500

    mar 3 inicio

---
+++ app/src/main/java/com/zea/company/route_u/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zea.company.route_u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/zea/company/route_u/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.zea.company.route_u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
